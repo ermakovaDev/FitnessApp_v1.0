d092814604cbd75766d1c4ec16a3803369ff4d8a
ermakovaDev <ermakova.dev@yandex.ru>
2022-10-13 17:10:05 +0300

    Revert "trouble"
    This reverts commit 41a25c27ac3a87db1e026d58c87820fedef69b17.

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -1,40 +1,11 @@
 package com.example.fitnessapp
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.fitnessapp.databinding.ActivityMainBinding
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-    val navController = findNavController(R.id.nav_host_fragment_content_main)
-    return navController.navigateUp(appBarConfiguration)
-            || super.onSupportNavigateUp()
+        setContentView(R.layout.activity_main)
     }
 }
