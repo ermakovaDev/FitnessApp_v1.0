9a9d3b8881c84873dcd1887e2b2ee23aa856bfb0
ermakovaDev <ermakova.dev@yandex.ru>
2022-10-14 15:00:16 +0300

    add DaysAdapter

---
+++ app/src/main/java/com/example/fitnessapp/adapters/DaysAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.fitnessapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fitnessapp.R
+import com.example.fitnessapp.databinding.DaysListItemBinding
+
+class DaysAdapter : ListAdapter<DayModel, DaysAdapter.DayHolder>(MyComparator()) { // класс модели + класс разметки
+
+    class DayHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = DaysListItemBinding.bind(view)
+
+        fun setData(day: DayModel) = with(binding) { // | binding.apply{}
+            val dayStr = root.context.getString(R.string.day) + " ${adapterPosition + 1}"
+            tvDays.text = dayStr
+            val exerciseCounter = day.exercises.split("_").size.toString()
+            tvCounter.text = exerciseCounter
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.days_list_item,parent,false)
+        return DayHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DayHolder, position: Int) {
+        holder.setData(getItem(position))
+    }
+
+    class MyComparator : DiffUtil.ItemCallback<DayModel>(){
+        override fun areItemsTheSame(oldItem: DayModel, newItem: DayModel): Boolean {
+       return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: DayModel, newItem: DayModel): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+}
